59ab081a13ea4cd40bb9236541d1a546b5bf96be
Unknown <sailen.nair@gmail.com>
2017-10-13 09:17:44 +0200

    Doxygen commenting updated to Where William did

--- EnemyPresentation.h
+++ EnemyPresentation.h
@@ -16,15 +16,45 @@ using namespace std;
 class EnemyPresentation
 {
 public:
-
+/**
+ * Default constructor Inititalise the player texture, default position and scale. 
+ */
     EnemyPresentation();
+    /**
+     * @brief Constructor Inititalise the player texture, default position and scale for a type of enemy.
+     * @param window the window to draw the enemy into.
+     */
     EnemyPresentation(int type);
     void draw(RenderWindow& window);
+    /**
+     * @brief Updates the enemy Sprite's position after the enemy has moved.
+     * @param xPosition  x co-ordinate the enemy has moved to.
+     * @param yPosition y co-ordinate the enemy has moved to.
+     */
     void updateEnemy(float xPosition, float yPosition);
+    /**
+     * @brief Returns the enemy Sprite.
+     * @return Sprite containing the enemy graphics.
+     */
     Sprite getEnemySprite();
+    /**
+     * @brief Updates the position of an enemy bullet.
+     * @param xPosition x co-ordinate the bullet has moved to.
+     * @param yPosition
+     */
     void updateEnemyBullet(float xPosition, float yPosition);
+    /**
+     * @brief Moves enemy Sprite to the center of the screen
+     */
     void moveToCenter();
+    /**
+     * @brief Returns a vector of EnemyBulletPresentation.
+     * @return EnemyBulletPresentation contianing the graphics of all enemy bullets
+     */
     vector<EnemyBulletPresentation> getEnemyBulletPresentationVector();
+    /**
+     * @brief Deletes the EnemyBulletPresentation vector
+     */
     void deleteEnemyBulletPresentation();
    
 private:
--- PlayerBullet.h
+++ PlayerBullet.h
@@ -7,17 +7,64 @@
 class PlayerBullet : public IBullet
 {
 public:
+/**
+ * @brief Constructor Initializes the Player bullets position, angle of direction and the the type of bullet
+ */
     PlayerBullet(int xPosition, int yPosition, float theta, int type);
+/**
+ * @brief Moves the bullet in the direction defined by the angle of direction
+ */
     void move();
+/**
+ * @brief Sets the damage that the bullet makes to an Enemy's health
+ * @param damage integer containing the damage amount
+ */
     void setDamage(int damage);
+/**
+ * @brief Gets the damage amount that the bullet makes to an enemy. 
+ * @return integer containing the damage amount
+ */    
     int getDamage();
+/**
+ * @brief Returns the life status of a bullet
+ * A bullets alive is defined by collisions. If a bullet has not collided its alive and if it has then it is dead.
+ * @return boolean contatning the life status of the bulllet
+ */
     bool isAlive();
+/**
+ * @brief Returns the x co-ordinate of the bullets current position
+ * @return float containing the x co-ordinate of the bullet
+ */
     float getXposition();
+/**
+ * @brief Returns the y co-ordinate of the bullets current position
+ * @return float containing the y co-ordinate of the bullet
+ */
     float getYposition();
+/**
+ * @brief Sets the life status of the bullet
+ * @see isAlive()
+ * @param life boolean containing the life status the bullet should be set to. 
+ */
     void setLife(bool life);
+/**
+ * @brief Moves the bullet in the direction of the initialized angle 
+ */
     void fire();
+/**
+ * @brief Gets the raidus of the bullet
+ * @return integer containing the radius of the bullet
+ */
     float getRadius();
+/**
+ * @brief Gets the x co-ordinate of the center of the bullet
+ * @return float containing the x co-orindate of the center of the bullet
+ */
     float getCenterXPosition();
+/**
+ * @brief Gets the y co-ordinate of the center of the bullet
+ * @return float containing the y co-orindate of the center of the bullet
+ */
     float getCenterYPosition();
 
 private:
